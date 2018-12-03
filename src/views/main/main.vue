f9597292243b0d1a85ce4bedea72db92ba697a71
unknown <fanxiaoguang@tycmc.net.cn>
2018-12-03 16:49:06 +0800

    修改了main.vue 样式

--- src/views/main/main.vue
+++ src/views/main/main.vue
@@ -1,47 +1,47 @@
 <template>
   <el-container class="wrapper">
-    <el-aside width="150px">
-      <el-menu
-        :default-active="activeIndex"
-        router
-        class="menuBar"
-        @select="handleSelect"
-      >
-        <el-menu-item
-          index="1"
-          route="/"
-        >数据服务中心</el-menu-item>
-        <el-menu-item
-          index="2"
-          route="/resource"
-        ><i class="fa fa-tasks"></i>资源目录</el-menu-item>
-        <el-menu-item
-          index="3"
-          route="/createcode"
-        ><i class="fa fa-codepen"></i>代码生成</el-menu-item>
-      <el-menu-item
-          index="4"
-          route="/demo"
-        ><i class="fa fa-page"></i>模版demo</el-menu-item>
-      </el-menu>
-    </el-aside>
+    <el-header class="header-bar">
+      <!-- <img src="" alt="logo" class="header-logo"> -->
+      <div class="fl logo">数据服务中心</div>
+      <ul class="header-operations">
+        <li>
+          <i class="fa fa-user-circle-o"></i>
+          <i class="rootname">root</i>
+        </li>
+        <li class="xian"></li>
+        <li>
+          <i class="fa fa-home"></i>
+        </li>
+        <li class="xian"></li>
+        <li>
+          <i class="fa fa-barcode"></i>
+        </li>
+        <li class="xian"></li>
+        <li>
+          <i class="fa fa-power-off"></i>
+        </li>
+      </ul>
+    </el-header>
+    <el-container class="wrapper">
+      <el-aside width="150px">
+        <el-menu :default-active="activeIndex" router class="menuBar" @select="handleSelect">
+          <el-menu-item index="1" route="/resource">
+            <i class="fa fa-tasks"></i>资源目录
+          </el-menu-item>
+          <el-menu-item index="2" route="/createcode">
+            <i class="fa fa-codepen"></i>代码生成
+          </el-menu-item>
+          <el-menu-item index="3" route="/demo">
+            <i class="fa fa-page"></i>模版demo
+          </el-menu-item>
+        </el-menu>
+      </el-aside>
 
-    <el-container>
-      <el-header class="header-bar">
-        <!-- <img src="" alt="logo" class="header-logo"> -->
-        <ul class="header-operations">
-          <li><i class="fa fa-user-circle-o"></i> <i class="rootname">root</i></li>
-          <li class="xian"></li>
-          <li><i class="fa fa-home"></i></li>
-          <li class="xian"></li>
-          <li><i class="fa fa-barcode"></i></li>
-          <li class="xian"></li>
-          <li><i class="fa fa-power-off"></i></li>
-        </ul>
-      </el-header>
-      <el-main>
-        <router-view />
-      </el-main>
+      <el-container>
+        <el-main>
+          <router-view/>
+        </el-main>
+      </el-container>
     </el-container>
   </el-container>
 </template>
@@ -52,7 +52,7 @@ export default {
   components: {},
   data() {
     return {
-      activeIndex: "2",
+      activeIndex: "1",
       keyIndex: ""
     };
   },
@@ -60,9 +60,6 @@ export default {
 
   methods: {
     handleSelect(key, keyPath) {
-      if (key == "1") {
-        key = "2";
-      }
       this.keyIndex = key;
       console.log(key, keyPath);
     }
@@ -73,3 +70,19 @@ export default {
   mounted() {}
 };
 </script>
+<style>
+.fl {
+  float: left;
+}
+.el-header {
+  padding-left: 0 !important;
+}
+.logo {
+  width: 149px;
+  height: 60px;
+  line-height: 60px;
+  background: #515a6e;
+  text-align: center;
+  color: #fff;
+}
+</style>
