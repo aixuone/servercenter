326321c2e06117cb9c585fb7602c417505e506af
zhangxunan <554163483@qq.com>
2018-12-04 16:53:17 +0800

    webpack配置

--- src/libs/axios.js
+++ src/libs/axios.js
@@ -1,42 +1,43 @@
 import axios from 'axios';
 import { Message } from 'element-ui';
 // import store from "../store";
-const api_base_url = "http://192.168.18.188:19081";
-// const api_base_url = "http://localhost:19081";
+var API_ROOT;
+if (process.env.NODE_ENV=='development') {
+    API_ROOT = "http://192.168.18.188:19081";
+}else{
+    API_ROOT = "http://localhost:19081";
+}
 axios.defaults.headers['Content-Type'] = 'application/json;charset=UTF-8';
 const instance = axios.create({
-    baseURL: api_base_url,
+    baseURL: API_ROOT,
     //跨站访问
     // withCredentials: true
 });
-  
-  instance.interceptors.request.use(
+
+instance.interceptors.request.use(
     config => {
-      //  config.headers.Authorization = store.state.user.token;
-      // Spin.show()
-      // 在发送请求之前做些什么
-      return config;
+        //  config.headers.Authorization = store.state.user.token;
+        // Spin.show()
+        // 在发送请求之前做些什么
+        return config;
     },
     error => {
-      // 对请求错误做些什么
-      return Promise.reject(error);
+        // 对请求错误做些什么
+        return Promise.reject(error);
     }
-  );
-  
-  instance.interceptors.response.use(
-    res => {
-        console.log("res",res);
-      // console.log(res);
-    //   if (res.data !== "" && res.data.code !== 200) {
-        if(res.data.success !== true){
-            console.log("res.data.success",res);
-            Message.error("传输错误，"+res.data.code);
-            return Promise.reject(res);
-        }
+    );
+    
+    instance.interceptors.response.use(
+        res => {        
+            //   if (res.data !== "" && res.data.code !== 200) {
+                if(res.data.success !== true){
+                    Message.error("传输错误，"+res.data.code);
+                    return Promise.reject(res);
+                }
         return res;
     },
     error => {
-        console.log("error",error);
+        console.log("error->",error);
       if (!error.success) {
         Message.error("网络错误");
       } else if (error.response.data.code >= 500) {
